337bd4dffba43553c640a7081d428063e6a540c7
sanjayyadav <102154219+sanjaypkumar@users.noreply.github.com>
2024-04-17 20:43:59 +0530

    first health care commit

--- src/main/java/in/project/sanjay/entity/Specialization.java
+++ src/main/java/in/project/sanjay/entity/Specialization.java
@@ -1,5 +1,27 @@
 package in.project.sanjay.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="specialization_tab")
 public class Specialization {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="spec_id_col")
+	private Long id;
+	
+	@Column(name="spec_code_col")
+	private String code;
+	@Column(name="spec_name_col")
+	private String name;
+	@Column(name="spec_note_col")
+	private String note;
 }
