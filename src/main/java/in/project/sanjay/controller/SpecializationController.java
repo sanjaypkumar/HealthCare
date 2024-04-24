829743a01ae798e09117ce6bd336c6aff0aca711
sanjayyadav <102154219+sanjaypkumar@users.noreply.github.com>
2024-04-24 18:02:15 +0530

    register form, validation form for database & html

--- src/main/java/in/project/sanjay/controller/SpecializationController.java
+++ src/main/java/in/project/sanjay/controller/SpecializationController.java
@@ -3,30 +3,59 @@ package in.project.sanjay.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import in.project.sanjay.entity.Specialization;
 import in.project.sanjay.service.ISpecializationService;
 
 @Controller
 @RequestMapping("/spec")
+@Configuration
 public class SpecializationController {
 
 	@Autowired
 	private ISpecializationService service;
 	
 	/*
-	 * 1.Display all Specializations
+	 * 1. show Register Page
 	 */
+	@GetMapping("/register")
+	public String showRegister() {
+		return "SpecializationRegister";
+	}
 	
+	
+	/*
+	 * 2.On submit form save Data
+	 */
+	@GetMapping("/save")
+	public String saveForm(
+			@ModelAttribute Specialization specialization,
+			Model model)
+	{
+		Long id = service.saveSpecialization(specialization);
+		String message = "Record ("+id+") is created";
+		model.addAttribute("message", message);
+		return "SpecializationRegister";
+	}
+	
+	
+	/*
+	 * 3.Display all Specializations
+	 */
 	@GetMapping("/all")
 	public String viewAll(Model model) {
 		List<Specialization> list = service.getAllSpecializations();
 		model.addAttribute("list", list);
 		return "Specialization";
 	}
+	
 	
 }
--- src/main/java/in/project/sanjay/entity/Specialization.java
+++ src/main/java/in/project/sanjay/entity/Specialization.java
@@ -22,10 +22,26 @@ public class Specialization {
 	@Column(name="spec_id_col")
 	private Long id;
 	
-	@Column(name="spec_code_col")
+	@Column(
+			name="spec_code_col",
+			length=12,  	 // default size is 255
+			nullable=false,  //default value is true, null values are accepted
+			unique = true   //default value is false, duplicates are allowed
+			)
 	private String specCode;
-	@Column(name="spec_name_col")
+	
+	@Column(
+			name="spec_name_col",
+			length=60,		// default size is 255
+			nullable=false,	//default value is true, null values are accepted
+			unique = true	//default value is false, duplicates are allowed
+			)
 	private String specName;
-	@Column(name="spec_note_col")
+	
+	@Column(
+			name="spec_note_col",
+			length=250, 			// default size is 255
+			nullable=false			//default value is true, null values are accepted
+			)
 	private String specNote;
 }
