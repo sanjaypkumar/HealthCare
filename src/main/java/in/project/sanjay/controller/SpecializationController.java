72448624500f7d1e2c39464139072f59bd6eede3
sanjayyadav <102154219+sanjaypkumar@users.noreply.github.com>
2024-04-28 23:54:50 +0530

    DB to excel Data

--- src/main/java/in/project/sanjay/controller/SpecializationController.java
+++ src/main/java/in/project/sanjay/controller/SpecializationController.java
@@ -3,7 +3,6 @@ package in.project.sanjay.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +11,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import in.project.sanjay.entity.Specialization;
 import in.project.sanjay.execption.SpecializationNotFoundExecption;
 import in.project.sanjay.service.ISpecializationService;
+import in.project.sanjay.view.SpecializationExcelView;
 
 @Controller
 @RequestMapping("/spec")
@@ -166,5 +166,19 @@ public class SpecializationController {
 			message = name +", *Already Exist";
 		}
 		return message;  //This time its not a viewname(its a message)
+	}
+	
+	/*
+	 * 8. Export Data to Excel file
+	 */
+	@GetMapping("/excel")
+	public ModelAndView exportToExcel() {
+		ModelAndView m = new ModelAndView();
+		m.setView(new SpecializationExcelView());
+		//Read data from DB
+		List<Specialization> list = service.getAllSpecializations();
+		//Send Data to Excel Impl class
+		m.addObject("list", list);
+		return  m;
 	}
 }
---
+++ src/main/java/in/project/sanjay/view/SpecializationExcelView.java
@@ -0,0 +1,63 @@
+package in.project.sanjay.view;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.web.servlet.view.document.AbstractXlsView;
+
+import in.project.sanjay.entity.Specialization;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SpecializationExcelView extends AbstractXlsView {
+
+	@Override
+	protected void buildExcelDocument(
+			Map<String, Object> model,
+			Workbook workbook, 
+			HttpServletRequest request,
+			HttpServletResponse response) 
+					throws Exception {
+		// TODO Auto-generated method stub
+		
+		//1. define Your own excel file name
+		response.addHeader("content-Disposition", "attachment;filename=SPECS.xlsx");
+		
+		//2. read data  given by controller
+		List<Specialization> list = (List<Specialization>) model.get("list");
+		
+		//3.create one sheet
+		Sheet sheet = workbook.createSheet("Specialization");
+		
+		//4.create row#0 as header
+		setHead(sheet);
+		
+		//5.create row#1 onwards as List<T>
+		setBody(sheet,list);
+	}
+
+	private void setHead(Sheet sheet) {
+		// TODO Auto-generated method stub
+		Row row = sheet.createRow(0);
+		row.createCell(0).setCellValue("ID");
+		row.createCell(1).setCellValue("CODE");
+		row.createCell(2).setCellValue("NAME");
+		row.createCell(3).setCellValue("NOTE");
+	}
+	
+	private void setBody(Sheet sheet, List<Specialization> list) {
+		// TODO Auto-generated method stub
+		int rowNum = 1;
+		for(Specialization spec : list) {
+			Row row = sheet.createRow(rowNum++);
+			row.createCell(0).setCellValue(spec.getId());
+			row.createCell(1).setCellValue(spec.getSpecCode());
+			row.createCell(2).setCellValue(spec.getSpecName());
+			row.createCell(3).setCellValue(spec.getSpecNote());
+		}
+	}
+
+}
